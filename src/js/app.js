0cf30c63d5db6de4630ff628a33c1033f77b6a3d
Apiwat <nopapiwat@gmail.com>
2016-01-15 00:21:54 +0700

    Finish routing with home.html and about.html

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,11 @@
 var app = angular.module('rorpap', ['ngRoute']);
 
 app.config(function($routeProvider) {
-  $routeProvider.when('/', {
-    controller: 'HomeController',
-    templateUrl: 'views/home.html'
-  }).when('/about', {
-    controller: 'AboutController',
-    templateUrl: 'views/about.html'
-  }).otherwise({
-    redirectTo: '/'
-  });
+	$routeProvider.when('/',{
+		templateUrl: 'views/home.html',
+		controller: 'HomeController'
+	}).when('/About',{
+		templateUrl: 'views/about.html',
+		controller: 'AboutController'
+	})
 });
