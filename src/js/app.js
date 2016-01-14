d150f35ed6d7fbecb67271ceddadec28937c58e2
Supavit Kongwudhikunakorn <supavit_knot@yahoo.co.th>
2016-01-14 18:20:04 +0700

    knot's initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,12 @@
-var app = angular.module("rorpap",[]);
+var app = angular.module("rorpap", ['ngRoute']);
+app.config(function($routeProvider) {
+  $routeProvider.when('/', {
+    controller: 'HomeController',
+    templateUrl: 'home.html'
+  }).when('/about', {
+    controller: 'AboutController',
+    templateUrl: 'about.html'
+  }).otherwise({
+    redirectTo: '/'
+  });
+});
---
+++ src/js/services/about.js
@@ -0,0 +1,5 @@
+<div class="about">
+  <div class="container">
+
+  </div>
+</div>
