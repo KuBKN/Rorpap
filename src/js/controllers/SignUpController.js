b674fcb2f6154c8502064402465b20d6610681ad
Supavit Kongwudhikunakorn <supavit_knot@yahoo.co.th>
2016-01-19 19:42:51 +0700

    upload image function in signup page

--- src/js/controllers/SignUpController.js
+++ src/js/controllers/SignUpController.js
@@ -2,6 +2,14 @@ var app = angular.module('rorpap');
 
 app.controller('SignUpController', ['$scope', function($scope) {
     $scope.passwordPattern = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+    $scope.upload = function(){
+        var f = $document.getElementById('file').files[0],
+            r = new FileReader();
+        r.onloadend = function (e) {
+            var data = e.target.result;
+        };
+        r.readAsBinaryString(f);
+    };
 }]);
 
 /* Directive for date picker during user's Date of Birth selection */
