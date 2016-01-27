0eae44288fc792e6bfff835b2461c92c7feefe5d
Apiwat <nopapiwat@gmail.com>
2016-01-27 17:53:32 +0700

    Add map in myrequest and newrequest

--- src/Server/public/js/controllers/MyRequestController.js
+++ src/Server/public/js/controllers/MyRequestController.js
@@ -3,7 +3,7 @@ app.controller('MyRequestController', ['$scope', '$http', '$cookies', function($
 	$scope.load = function() {
 		$('.collapsible').collapsible({
         accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-        });
+    });
 	};
 	$scope.load();
 
@@ -85,23 +85,56 @@ app.controller('MyRequestController', ['$scope', '$http', '$cookies', function($
 	$scope.getAllQuests = function() {
 		var sender_id = $cookies.get('_id').replace(/\"/g, "");
 		$http.get('/api/request/' + sender_id)
-			.success(function(data) {
-				console.log(data);
+		.success(function(data) {
+			console.log(data);
 
-				angular.forEach(data, function(value, key) {
-					$scope.lists.push(value);
-				});
-
-				console.log($scope.lists);
-			})
-			.error(function(data) {
-				console.log(data);
+			angular.forEach(data, function(value, key) {
+				$scope.lists.push(value);
 			});
+
+			console.log($scope.lists);
+		})
+		.error(function(data) {
+			console.log(data);
+		});
 	}
 
 	// twice calling
 	$scope.getAllQuests();
 
-	$scope.map = { center: { latitude: 13.851648, longitude: 100.567465 }, zoom: 15 };
+	$scope.distance = function( val1, val2){
+	    console.log(val1);
+	    console.log(val2);
+	    return (val1+val2)/2;
+	  };
 
+	$scope.marker1 = {
+		id: 0,
+		coords: {
+			latitude: 13.851648,
+			longitude: 100.567465
+		},
+		options: { draggable: true,
+			icon: 'images/LOGO-RED.png'
+		}
+	};
+
+	$scope.marker2 = {
+		id: 0,
+		coords: {
+			latitude: 13.738432,
+			longitude: 100.530925
+		},
+		options: { draggable: true,
+			icon: 'images/LOGO-GREEN.png' 
+		}
+	};
+
+	$scope.map1 = { 
+	    center: { 
+	    latitude: $scope.distance($scope.marker1.coords.latitude,$scope.marker2.coords.latitude), 
+	    longitude: $scope.distance($scope.marker1.coords.longitude,$scope.marker2.coords.longitude)
+	    }, 
+	    zoom: 11
+	  };
 }]);
