b04a0c8cd8a91edca0579403045414fba91c409d
Poramate Homprakob <batmaster_kn@hotmail.com>
2016-01-28 14:33:53 +0700

    content beamessenger, fix conflict bamessenger and findrequest

--- src/Server/public/js/controllers/BeAMessengerController.js
+++ src/Server/public/js/controllers/BeAMessengerController.js
@@ -1,3 +1,32 @@
-app.controller('BeAMessengerController', ['$scope', function($scope){
+app.controller('BeAMessengerController', ['$scope', '$http', '$cookies', '$window', function($scope, $http, $cookies, $window) {
+
+    $scope.loadModal = function() {
+        $('.modal-trigger').leanModal({
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .5, // Opacity of modal background
+            in_duration: 300, // Transition in duration
+            out_duration: 200 // Transition out duration
+        });
+	};
+	$scope.loadModal();
+
+    $scope.user = {};
+    $scope._id = $cookies.get('_id');
+    if ($scope._id != undefined) {
+        $scope._id = $cookies.get('_id').replace(/\"/g,'');
+    }
+
+
+    $scope.load = function() {
+        $http.get('/api/user/' + $scope._id)
+			.success(function(data) {
+				$scope.user = data[0];
+                console.log(data);
+			})
+			.error(function(data) {
+				console.log(data);
+			});
+    }
+    $scope.load();
 
 }]);
--- src/Server/public/js/controllers/HeaderController.js
+++ src/Server/public/js/controllers/HeaderController.js
@@ -1,8 +1,25 @@
-app.controller('HeaderController',['$scope', '$cookies', function($scope, $cookies){
+app.controller('HeaderController', ['$scope', '$http', '$cookies', '$window', function($scope, $http, $cookies, $window) {
 
 	$scope.logIned = $cookies.get('_id') != undefined;
 
-	$scope.messenger = false;
+	$scope.user = {};
+    $scope._id = $cookies.get('_id');
+
+    if ($scope._id != undefined) {
+        $scope._id = $cookies.get('_id').replace(/\"/g,'');
+    }
+
+    $scope.load = function() {
+        $http.get('/api/user/' + $scope._id)
+			.success(function(data) {
+				$scope.user = data[0];
+                console.log(data);
+			})
+			.error(function(data) {
+				console.log(data);
+			});
+    }
+    $scope.load();
 
 	$scope.background = function(){
 		if($scope.messenger){
