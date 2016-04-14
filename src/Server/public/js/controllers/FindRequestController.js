c8fcacfe589d352523cdcd2636855efad993edef
Apiwat <nopapiwat@gmail.com>
2016-04-15 03:09:12 +0700

    prepare map for findrequest

--- src/Server/public/js/controllers/FindRequestController.js
+++ src/Server/public/js/controllers/FindRequestController.js
@@ -1,6 +1,6 @@
-app.controller('FindRequestController', ['$scope', '$http','$cookies', 'profileViewer', 'loadUser', 'requestColor', function($scope, $http, $cookies, profileViewer, loadUser, requestColor, uiGmapGoogleMapApi){
+app.controller('FindRequestController', ['$scope', '$http','$cookies', 'profileViewer', 'loadUser', 'requestColor', 'requestParcelImg', function($scope, $http, $cookies, profileViewer, loadUser, requestColor, requestParcelImg, uiGmapGoogleMapApi){
 
-	$scope.filter = { prices_max: 80, prices_min: 20 };
+	$scope.filter = { prices_max: 80, prices_min: 20, weight_max: 10, weight_min: 1 };
 	$scope.requests = [];
 
 	$scope.getRequests = function() {
@@ -25,6 +25,7 @@ app.controller('FindRequestController', ['$scope', '$http','$cookies', 'profileV
 							loadUser.getUser(value.sender_id).then(function(result){
 		   						value.sender = result;
 		   					});
+		   					value.pimg = requestParcelImg.getByIndex(value.img);
 							$scope.requests.push(value);
 						}
 					});					
@@ -42,7 +43,7 @@ app.controller('FindRequestController', ['$scope', '$http','$cookies', 'profileV
 			accordion : false
 		});
 
-	    var slider = document.getElementById('range-input');
+	    var slider = document.getElementById('price-input');
 	    noUiSlider.create(slider, {
 	     	start: [20, 80],
 	       	connect: true,
@@ -64,6 +65,30 @@ app.controller('FindRequestController', ['$scope', '$http','$cookies', 'profileV
 			}
 			$scope.getRequests();
 		});
+
+	    slider = document.getElementById('weight-input');
+	    noUiSlider.create(slider, {
+	     	start: [1, 10],
+	       	connect: true,
+	       	step: 0.1,
+	       	range: {
+	         'min': 0.1,
+	         'max': 15
+	       },
+	       format: wNumb({
+	         decimals: 0
+	       })
+	     });
+
+	    slider.noUiSlider.on('change', function( values, handle ) {	    		    	
+			if ( handle ) {				
+				$scope.filter.weight_max = parseFloat(values[handle]);
+			} else {
+				$scope.filter.weight_min = parseFloat(values[handle]);
+			}
+			$scope.getRequests();
+		});
+
 	};
 	$scope.load();
 
@@ -111,15 +136,110 @@ app.controller('FindRequestController', ['$scope', '$http','$cookies', 'profileV
     $scope.openModal = function(index){		       
         $('#modalAccept').openModal();
         $scope.curreq = $scope.requests[index];       
-    };    
+    };
 
+    $scope.calNear = function(req){
+
+    	return true;
+    };
+
+    $scope.path = [];
+    $scope.markers = [];
     $scope.filted = function(index){
-    	var req =  parseInt($scope.requests[index].price);
-    	return $scope.filter.prices_min <= req && req <= $scope.filter.prices_max;
+    	var price =  parseInt($scope.requests[index].price);
+    	var weight =  parseInt($scope.requests[index].weight);
+    	var near = true;
+    	if($scope.path.length != 0){
+    		near = $scope.calNear($scope.requests[index]);
+    	}
+    	return ($scope.filter.prices_min <= price && price <= $scope.filter.prices_max)
+    	 && ($scope.filter.weight_min <= weight && weight <= $scope.filter.weight_max)
+    	 && near;
     };
 
-    $scope.showInMap = function(index){    	
-    	console.log($scope.filter); 	
-    };
+    $scope.map = {
+		center: [
+			13.738432,
+			100.530925
+		],
+		zoom: 12,
+		zoomToIncludeMarkers: false
+	};	
+
+    $scope.updateMarkers = function(index){    	
+		var loc = $scope.requests[index].fromLoc.split(', ');		
+		$scope.markers.push({ pos: [loc[0],loc[1]],
+								optimized: true,
+								icon: { url:'images/LOGO-RED.png',
+						        		scaledSize:[40,40]
+						      	} 
+						    });
+		loc = $scope.requests[index].toLoc.split(', ');
+		$scope.markers.push({ pos: [loc[0],loc[1]],
+								optimized: true,
+								icon: { url:'images/LOGO-GREEN.png',
+						        		scaledSize:[40,40]
+						      	} 
+						    });
+	};
+
+    $scope.showInMap = function(index){
+	    console.log($scope.path);
+    	$scope.map.zoomToIncludeMarkers = 'auto';
+		$scope.markers = [];
+		if (index != $scope.lastCollepsed) {
+			$scope.updateMarkers(index);
+			$scope.lastCollepsed = index;
+		}
+		else {			
+			$scope.lastCollepsed = -1;
+			$scope.map.center = [13.738432, 100.530925];
+			$scope.map.zoom = 12;
+			$scope.map.zoomToIncludeMarkers = false;					
+		}
+	};
+	$scope.lastCollepsed = -1;
+
+    $scope.showFilter = false;
+    
+    $scope.countM = 0;
+    $scope.markerO = [];
+    $scope.$on('mapInitialized', function(event, map) {
+			 $scope.rmap = map;
+			 // $scope.rmap.directionsRenderers[0].setOptions({
+				//   map: map,
+				//   suppressMarkers : true
+				// });
+
+			 $scope.rmap.directionsRenderers[0].setOptions( { markerOptions: {
+			 	icon: 'images/LOGO-ORANGE.png' }
+			 } );
+		});
+    $scope.addMarker = function(event) {
+    	if($scope.countM < 2){
+			var e = event.latLng;
+			var pos = [e.lat(),e.lng()];
+			if($scope.countM==0){
+				var originM = {};
+				originM.pos = pos;
+				originM.optimized = false;
+				originM.icon = 'images/LOGO-GREEN.png'
+				$scope.markerO.push(originM);				
+				$scope.origin = pos;
+			}else{
+				$scope.des = pos;
+				$scope.markerO = [];
+				setTimeout(function(){
+				    $scope.path = $scope.rmap.directionsRenderers[0].directions.routes[0].overview_path;
+				    $scope.getRequests();
+				    console.log($scope.rmap)
+				}, 500);						
+			}
+			$scope.countM+=1;						
+		}
+
+	};
+
+
 
 }]);
