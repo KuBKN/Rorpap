e61e0eda9102e3c34f5e7ad2d9cdf739b2b76796
Poramate HomProkob <batmaster_kn@hotmail.com>
2016-04-19 21:29:15 +0700

    add price range

--- src/Server/public/js/controllers/NewRequestController.js
+++ src/Server/public/js/controllers/NewRequestController.js
@@ -2,7 +2,7 @@ app.controller('NewRequestController', ['$scope', '$http', '$cookies', '$locatio
 
 	$scope.load = function() {
 		$('select').material_select();
-		$('#mySelect').val();		
+		$('#mySelect').val();
 	};
 	$scope.load();
 
@@ -16,8 +16,6 @@ app.controller('NewRequestController', ['$scope', '$http', '$cookies', '$locatio
 	'48','49','50','51','52','53','54','55','56','57','58','59'];
 
 	$scope.request = {};
-	
-	$scope.request.price = 75;
 
 	$scope.get_id = function() {
 		var _id = $cookies.get('_id');
@@ -38,7 +36,7 @@ app.controller('NewRequestController', ['$scope', '$http', '$cookies', '$locatio
 
 	$scope.parcelImgs = requestParcelImg.getAll();
 
-	$scope.chooseImg = function(index){		
+	$scope.chooseImg = function(index){
 		$scope.request.img = index;
 	};
 
@@ -48,11 +46,11 @@ app.controller('NewRequestController', ['$scope', '$http', '$cookies', '$locatio
 		if (!$scope.checkLogin()){
 			return;
 		}
-				
+
 		$scope.request.fromLoc = $scope.markers[0].position[0] + ', ' + $scope.markers[0].position[1];
 		$scope.request.toLoc = $scope.markers[1].position[0] + ', ' + $scope.markers[1].position[1];
 		var d = new Date($scope.request.shipLimitDate_tmp);
-		$scope.request.shipLimitDate = ""+d.getDate()+"/"+(d.getMonth()+1)+"/"+d.getFullYear();		
+		$scope.request.shipLimitDate = ""+d.getDate()+"/"+(d.getMonth()+1)+"/"+d.getFullYear();
 		console.log($scope.request);
 		if($scope.editMode){
 			$http.post('/api/request/update', $scope.request)
@@ -77,7 +75,7 @@ app.controller('NewRequestController', ['$scope', '$http', '$cookies', '$locatio
 				console.log('Error: ' + data);
 			});
 		}
-		
+
 	};
 
 	$scope.markers = [];
@@ -104,11 +102,16 @@ app.controller('NewRequestController', ['$scope', '$http', '$cookies', '$locatio
 
 			$scope.markers.push(marker);
 		}
+		else {
+			$scope.setPrice();
+		}
 	}
 
 	$scope.drag = function(event,index){
 		var e = event.latLng;
 		$scope.markers[index].position = [e.lat(),e.lng()];
+
+		$scope.setPrice();
 	}
 
 	$scope.editMode = false;
@@ -146,6 +149,73 @@ app.controller('NewRequestController', ['$scope', '$http', '$cookies', '$locatio
 					return $scope.request.toLoc.split(', ')[1];
 				}
 			}
-		});	
+		});
 	};
+
+
+	// Price bar
+	$scope.priceMin = 0;
+	$scope.priceMax = 0;
+
+	var rad = function(x) {
+	  return x * Math.PI / 180;
+  	};
+
+	var getDistance = function(p1, p2) {
+	  var R = 6378137; // Earth’s mean radius in meter
+	  var dLat = rad(p2[0] - p1[0]);
+	  var dLong = rad(p2[1] - p1[1]);
+	  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+	    Math.cos(rad(p1[0]) * Math.cos(rad(p2[0]))) *
+	    Math.sin(dLong / 2) * Math.sin(dLong / 2);
+	  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	  var d = R * c;
+	  return d; // returns the distance in meter
+	};
+
+	var getPrice1 = function(dis, vol, day) {
+		return ((5 * dis) + (vol / 100)) / day;
+	};
+
+	var getPrice2 = function(dis, wei, day) {
+		return ((5 * dis) + (wei * 100)) / day;
+	};
+
+	$scope.setPrice = function() {
+		if ($scope.markers[0] != undefined && $scope.markers[1] != undefined &&
+		$scope.request.size_w != undefined && $scope.request.size_l != undefined && $scope.request.size_h != undefined &&
+		$scope.request.weight != undefined && $scope.request.shipLimitDate_tmp != undefined) {
+
+			var dis = getDistance($scope.markers[0].position, $scope.markers[1].position) / 1000;
+			var vol = $scope.request.size_w * $scope.request.size_l * $scope.request.size_h;
+			var wei = $scope.request.weight;
+			var day = Math.ceil(Math.abs($scope.request.shipLimitDate_tmp.getTime() - new Date().getTime()) / (1000 * 3600 * 24)); ;
+			var p1 = Math.ceil(getPrice1(dis, vol, day));
+			var p2 = Math.ceil(getPrice2(dis, wei, day));
+
+			var min = Math.min(p1, p2);
+			var max = Math.max(p1, p2);
+			
+			$scope.request.price = Math.ceil((min + max) / 2);
+			$scope.priceMin = min;
+			$scope.priceMax = max;
+
+			console.log("dis: " + dis);
+			console.log("vol: " + vol);
+			console.log("wei: " + wei);
+			console.log("day: " + day);
+			console.log("p1: " + p1);
+			console.log("p2: " + p2);
+			console.log("av: " + $scope.request.price);
+		}
+		else {
+			$scope.request.price = 0;
+			$scope.priceMin = 0;
+			$scope.priceMax = 0;
+		}
+	}
+
+
+
+
 }]);
